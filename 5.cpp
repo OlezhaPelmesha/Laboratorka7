c786c373966e2c77a28025b36a2fd11fe4922353
OlezhaPelmesha <pro100oleg1200@ukr.net>
2021-11-20 21:23:30 +0200

    Lubovnoe poslanie

--- 5.cpp
+++ 5.cpp
@@ -20,7 +20,7 @@ int main()
 	cout << "Введіть h ";
 	cin >> h;
 	x = minX;
-	
+	// ya lublu zheku i igora
 	while (x <= maxX + h / 2) {
 		y = pow(x, 8) + 5 * pow(x, 5) - 5;
 		cout << setw(10) << left << x << setw(8) << right << y << endl;
